e9c321324ed74e6832b1f2c9ffbf6e75325cf754
Yonatan Lahav <jonathanla180@gmail.com>
2022-06-10 14:38:15 +0300

    add repo, chat screen and adapters

--- app/src/main/java/com/example/whatsapp/entities/ApiMessage.java
+++ app/src/main/java/com/example/whatsapp/entities/ApiMessage.java
@@ -1,2 +1,50 @@
-package com.example.whatsapp.entities;public class ApiMessage {
+package com.example.whatsapp.entities;
+
+
+import androidx.room.PrimaryKey;
+
+public class ApiMessage {
+    @PrimaryKey(autoGenerate = true)
+    private int _id;
+    private String _content;
+    private String _created;
+    private boolean _sent;
+
+    public ApiMessage(String content, String created, boolean sent) {
+        this._content = content;
+        this._created = created;
+        this._sent = sent;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
+
+    public String get_created() {
+        return _created;
+    }
+
+    public void set_created(String _created) {
+        this._created = _created;
+    }
+
+    public boolean is_sent() {
+        return _sent;
+    }
+
+    public void set_sent(boolean _sent) {
+        this._sent = _sent;
+    }
 }
--- app/src/main/java/com/example/whatsapp/repositories/ApiContactRepository.java
+++ app/src/main/java/com/example/whatsapp/repositories/ApiContactRepository.java
@@ -1,4 +1,72 @@
 package com.example.whatsapp.repositories;
 
-public class ApiContactsRepository {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.whatsapp.entities.ApiContact;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ApiContactRepository {
+    //    private ApiContactDao dao;
+//    private ApiContactApi api;
+    private ApiContactsListData _apiContactsListData;
+
+    public ApiContactRepository() {
+//        LocalDatabase db = LocalDatabase.getInstance();
+//        dao = db.apiContactDao();
+//        api = new ApiContactApi();
+        _apiContactsListData = new ApiContactsListData();
+    }
+
+    private class ApiContactsListData extends MutableLiveData<List<ApiContact>> {
+        public ApiContactsListData() {
+            super();
+            List<ApiContact> apiContacts = new LinkedList<>();
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("admin", "admin", "localhost", "Hey", "10:00"));
+            apiContacts.add(new ApiContact("yonatan", "yonatan", "localhost", "Heeeeeeeeeee", "10:00"));
+            apiContacts.add(new ApiContact("roy", "roy", "localhost", "Hellooo", "10:00"));
+            apiContacts.add(new ApiContact("liron", "liron", "localhost", "Hey", "10:00"));
+            setValue(apiContacts);
+//            setValue(new LinkedList<>());
+        }
+
+        @Override
+        protected void onActive() {
+            super.onActive();
+
+//            new Thread(()->{
+//                _apiContactsListData.postValue(dao.get());
+//            }).start();
+        }
+    }
+
+    public LiveData<List<ApiContact>> getAll() {
+        return _apiContactsListData;
+    }
 }
