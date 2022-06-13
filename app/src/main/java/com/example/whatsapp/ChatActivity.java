4e19dcd9aa9184c4a7592f5587ec7237fbedb4a1
Your Name <roey.maman@gmail.com>
2022-06-13 17:03:29 +0300

    landspace

--- app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -1,16 +1,22 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.whatsapp.adapters.ApiContactListAdapter;
 import com.example.whatsapp.adapters.ApiMessageListAdapter;
+import com.example.whatsapp.entities.LoginPostRequest;
+import com.example.whatsapp.interfaces.ListItemClickListener;
 import com.example.whatsapp.viewmodels.ApiContactViewModel;
 import com.example.whatsapp.viewmodels.ApiMessageViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -34,5 +40,6 @@ public class ChatActivity extends AppCompatActivity {
         viewModel.get().observe(this, apiMessages -> {
             adapter.setMessages(apiMessages);
         });
+
     }
 }
